f59b81ecd240ae284e8bba421b8bf268ae800b11
emrecanAy <codemrecan@gmail.com>
2021-12-04 17:30:59 +0300

    Courses are listed on Courses page!

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -3,15 +3,15 @@ const Course = require('../models/Course');
 exports.getAllCourses = async (req, res) => {
   try {
     const courses = await Course.find();
-    res.status(200).json({
-      status: 'success',
+    res.status(200).render('courses', {
       courses,
+      page_name: 'courses'
     });
   } catch (error) {
     res.status(400).json({
-        status: 'failed',
-        error,
-      });
+      status: 'failed',
+      error,
+    });
   }
 };
 
