ae272b493e7d70e91fecee0438407ac49e62feff
emrecanAy <codemrecan@gmail.com>
2021-12-04 21:39:49 +0300

    course pages and slugify implemented!

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route('/').post(coursesController.createCourse);
 router.route('/').get(coursesController.getAllCourses);
+router.route('/:slug').get(coursesController.getCourse);
 
 module.exports = router;
